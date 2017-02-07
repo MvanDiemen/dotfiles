1bc0ea7fe8768ce1a7647c4d841b3f2a4d14851b
Michaël van Diemen <van.diemen.michael@gmail.com>
2017-02-07 17:00:57 +0100

    Use xfreerdp

--- .config/fish/functions/flex_rdp.fish
+++ .config/fish/functions/flex_rdp.fish
@@ -1,3 +1,3 @@
 function flex_rdp -d "Remote Desktop to Flexservice"
-  rdesktop -g 1440x900 -P -z -x l -r sound:off -d CAREERNETWORK -u mvandiemen 10.20.16.44
+  xfreerdp -grab-keyboard -themes -wallpaper +clipboard /size:1920x1040 /v:10.20.16.46 /d:CAREERNETWORK /u:mvandiemen
 end
