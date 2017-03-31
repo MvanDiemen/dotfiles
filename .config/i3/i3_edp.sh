41c8d03063555c07f6aff5631704ac137c951ed9
Michaël van Diemen <van.diemen.michael@gmail.com>
2017-03-31 16:51:20 +0200

    Just put them in both..

--- .config/i3/i3_edp.sh
+++ .config/i3/i3_edp.sh
@@ -4,13 +4,13 @@ if xrandr --query | grep "HDMI1 connected"; then
   if xrandr --query | grep "HDMI2 connected"; then
     echo "2 external HDMI monitors found"
     xrandr --auto --output "HDMI2" --dpi 96 --mode "1920x1080" --output "eDP1" --off --output "HDMI1" --off
-    # --auto --output "HDMI-2" --mode "1920x1080" --right-of "HDMI-1"
+    # xrandr --auto --output "HDMI2" --dpi 96 --mode "1920x1080" --output "eDP1" --off --auto --output "HDMI1" --mode "1920x1080" --left-of "HDMI2"
   fi
 elif xrandr --query | grep "HDMI-1 connected"; then
   if xrandr --query | grep "HDMI-2 connected"; then
     echo "2 external HDMI- monitors found"
     xrandr --auto --output "HDMI-2" --dpi 96 --mode "1920x1080" --output "eDP-1" --off --auto --output "HDMI-1" --off
-    #--output "HDMI-2" --mode "1920x1080" --right-of "HDMI-1"
+    # xrandr --auto --output "HDMI-2" --dpi 96 --mode "1920x1080" --output "eDP-1" --off --auto --output "HDMI-1" --mode "1920x1080" --left-of "HDMI-2"
   fi
 elif xrandr --query | grep "^DP-1 connected"; then
   echo "1 external DP monitor found"
