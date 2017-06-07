7cdb6ac5a554c95fb0570f5ca324eb36d1027bcf
Michael van Diemen <van.diemen.michael@gmail.com>
2017-06-07 11:45:38 +0200

    Monitor changes for new Dell

--- .config/i3/i3_edp.sh
+++ .config/i3/i3_edp.sh
@@ -1,27 +1,10 @@
 #!/bin/bash
-
-if xrandr --query | grep "HDMI1 connected"; then
-  if xrandr --query | grep "HDMI2 connected"; then
-    echo "2 external HDMI monitors found"
-    xrandr --auto --output "HDMI2" --dpi 96 --mode "1920x1080" --output "eDP1" --off --output "HDMI1" --off
-    # xrandr --auto --output "HDMI2" --dpi 96 --mode "1920x1080" --output "eDP1" --off --auto --output "HDMI1" --mode "1920x1080" --left-of "HDMI2"
+if xrandr --query | grep "HDMI-1 connected"; then
+  if xrandr --query | grep "3840x2160"; then
+    xrandr --auto --output "HDMI-1" --dpi 120 --mode "3840x2160" --output "eDP1" --off
+  else
+    xrandr --auto --output "HDMI-1" --dpi 96 --mode "1920x1080" --output "eDP-1" --off --auto
   fi
-elif xrandr --query | grep "HDMI-1 connected"; then
-  if xrandr --query | grep "HDMI-2 connected"; then
-    echo "2 external HDMI- monitors found"
-    xrandr --auto --output "HDMI-2" --dpi 96 --mode "1920x1080" --output "eDP-1" --off --auto --output "HDMI-1" --off
-    # xrandr --auto --output "HDMI-2" --dpi 96 --mode "1920x1080" --output "eDP-1" --off --auto --output "HDMI-1" --mode "1920x1080" --left-of "HDMI-2"
-  fi
-elif xrandr --query | grep "^DP-1 connected"; then
-  echo "1 external DP monitor found"
-  xrandr --auto --output "DP-1" --dpi 120 --mode "3840x2160" --output "eDP-1" --off
-elif xrandr --query | grep "^DP1 connected"; then
-  echo "1 external DP monitor found"
-  xrandr --auto --output "DP1" --dpi 120 --mode "3840x2160" --output "eDP1" --off
-elif xrandr --query | grep "eDP-1 connected"; then
-  echo "no external monitor found"
-  xrandr --auto --output "eDP-1" --dpi 120 --mode "2560x1600"
 else
-  echo "no external monitor found"
-  xrandr --auto --output "eDP1" --dpi 120 --mode "2560x1600"
+  xrandr --auto --output "eDP-1" --dpi 96 --mode "1920x1080"
 fi
