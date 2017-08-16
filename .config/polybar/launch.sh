46fa77200c578bbcbc08c01d201a9302d885aceb
Michael van Diemen <van.diemen.michael@gmail.com>
2017-08-16 13:48:25 +0200

    Setup polybar for all monitors

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -6,11 +6,20 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -x polybar >/dev/null; do sleep 1; done
 
+
 # Launch bar1 and bar2
 if xrandr --query | grep "3840x2160"; then
   polybar top4k
 else
-  polybar top
+  # Launch bar1 and bar2
+  DISPLAY1="$(xrandr -q | grep 'DP-1-2' | cut -d ' ' -f1)"
+  [[ ! -z "$DISPLAY1" ]] && MONITOR="$DISPLAY1" polybar top &
+
+  DISPLAY3="$(xrandr -q | grep 'eDP-1' | cut -d ' ' -f1)"
+  [[ ! -z "$DISPLAY3" ]] && MONITOR="$DISPLAY3" polybar top &
+
+  DISPLAY2="$(xrandr -q | grep 'DP-1-1' | cut -d ' ' -f1)"
+  [[ ! -z "$DISPLAY2" ]] && MONITOR="$DISPLAY2" polybar top &
 fi
 
 echo "Bars launched..."
