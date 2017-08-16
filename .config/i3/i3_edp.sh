d84ed5959a5fccefedc3a21a6a1cfb7dfdd4ecb2
Michael van Diemen <van.diemen.michael@gmail.com>
2017-08-16 13:45:57 +0200

    Screen script for 1, 2 and 3 monitors

--- .config/i3/i3_edp.sh
+++ .config/i3/i3_edp.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
-if xrandr --query | grep "HDMI-1 connected"; then
-  if xrandr --query | grep "3840x2160"; then
-    xrandr --auto --output "HDMI-1" --dpi 120 --mode "3840x2160" --output "eDP1" --off
+if xrandr --query | grep "DP-1-2 connected"; then
+  if xrandr --query | grep "DP-1-1 connected"; then
+    xrandr --auto --output "DP-1-2" --dpi 96 --mode "1920x1080" --output "DP-1-1" --mode "1920x1080" --left-of "DP-1-2" --output "eDP-1" --off --auto
+    # xrandr --auto --output "DP-1-2" --dpi 96 --mode "1920x1080" --output "eDP-1" --off --auto --output "DP-1-1" --off --auto
   else
-    xrandr --auto --output "HDMI-1" --dpi 96 --mode "1920x1080" --output "eDP-1" --off --auto
+    xrandr --auto --output "DP-1-2" --dpi 96 --mode "1920x1080" --output "eDP-1" --off --auto --output "DP-1-1" --off --auto
   fi
+elif xrandr --query | grep "HDMI-1 connected"; then
+  xrandr --auto --output "HDMI-1" --dpi 120 --mode "3840x2160" --output "eDP-1" --off --auto
 else
   xrandr --auto --output "eDP-1" --dpi 96 --mode "1920x1080"
 fi
