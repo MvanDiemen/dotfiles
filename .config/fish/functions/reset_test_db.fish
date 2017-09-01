0376f894772ac4815c8aa0e5f38f5eecd0e54aa9
Michael van Diemen <van.diemen.michael@gmail.com>
2017-09-01 09:51:05 +0200

    bundle exec for test db

--- .config/fish/functions/reset_test_db.fish
+++ .config/fish/functions/reset_test_db.fish
@@ -1,3 +1,3 @@
 function reset_test_db -d 'Reset the test env database'
-  env RAILS_ENV=test rake db:drop db:create db:test:prepare db:migrate
+  env RAILS_ENV=test bundle exec rake db:drop db:create db:test:prepare db:migrate
 end
