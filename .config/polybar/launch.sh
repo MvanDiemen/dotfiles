f5a820d9380bd26899f6a0ba9a28f5916b0c7ac1
Michael van Diemen <michael.vandiemen@tellcharlie.com>
2017-11-23 15:29:54 +0100

    Adjusted polybar for colors and screens

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -6,20 +6,17 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -x polybar >/dev/null; do sleep 1; done
 
-
-# Launch bar1 and bar2
 if xrandr --query | grep "3840x2160"; then
   polybar top4k
 else
-  # Launch bar1 and bar2
-  DISPLAY1="$(xrandr -q | grep 'DP-1-2' | cut -d ' ' -f1)"
-  [[ ! -z "$DISPLAY1" ]] && MONITOR="$DISPLAY1" polybar top &
+  DISPLAY1="$(xrandr -q | grep 'HDMI-1 connected' | cut -d ' ' -f1)"
+  [[ ! -z "$DISPLAY1" ]] && MONITOR="$DISPLAY1" polybar 4ktop &
 
-  DISPLAY3="$(xrandr -q | grep 'eDP-1' | cut -d ' ' -f1)"
+  DISPLAY2="$(xrandr -q | grep 'eDP-1 connected' | cut -d ' ' -f1)"
+  [[ ! -z "$DISPLAY2" ]] && MONITOR="$DISPLAY2" polybar laptop &
+
+  DISPLAY3="$(xrandr -q | grep 'DP-2 connected' | cut -d ' ' -f1)"
   [[ ! -z "$DISPLAY3" ]] && MONITOR="$DISPLAY3" polybar top &
-
-  DISPLAY2="$(xrandr -q | grep 'DP-1-1' | cut -d ' ' -f1)"
-  [[ ! -z "$DISPLAY2" ]] && MONITOR="$DISPLAY2" polybar top &
 fi
 
 echo "Bars launched..."
