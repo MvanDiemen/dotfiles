aec61e3af9db4be9f0b5f388547e193f19988575
Michael van Diemen <michael.vandiemen@tellcharlie.com>
2018-04-12 15:10:17 +0200

    Use HDMI for 2k output

--- .config/i3/i3_edp.sh
+++ .config/i3/i3_edp.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 if xrandr --query | grep "HDMI-1 connected"; then
-    xrandr --auto --output "HDMI-1" --dpi 120 --mode "3840x2160" --output "eDP-1" --off --auto
+    # xrandr --auto --output "HDMI-1" --dpi 120 --mode "3840x2160" --output "eDP-1" --off --auto
+    xrandr --auto --output "HDMI-1" --dpi 96 --mode "2560x1440" --output "eDP-1" --off --auto
     # /usr/local/libexec/hotplug_polybar.sh
     /usr/bin/notify-send --urgency=low -t 500 "Switched to HDMI-1"
 elif xrandr --query | grep "DP-2 connected"; then
