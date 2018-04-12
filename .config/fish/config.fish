eb792c733d72c8d44725ce738aab5a8ec88e72c3
Michael van Diemen <michael.vandiemen@tellcharlie.com>
2018-04-12 15:08:34 +0200

    Adjusted background and theme stuff for fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -33,7 +33,7 @@ set fish_color_comment 9e9e9e
 set fish_color_operator blue
 set fish_color_redirection yellow
 set fish_color_end yellow
-set fish_color_search_match --background=333333
+set fish_color_search_match --background=555
 set fish_color_valid_path
 set fish_color_match yellow
 set fish_color_autosuggestion $fish_color_comment
@@ -42,6 +42,11 @@ set fish_pager_color_prefix yellow
 set fish_pager_color_progress yellow
 set fish_pager_color_completion $fish_color_redirection
 set fish_pager_color_description $fish_color_comment
+
+set -g theme_nerd_fonts no
+set -g fish_prompt_pwd_dir_length 1
+set -g theme_display_ruby no
+set -g theme_project_dir_length 1
 
 if test -e $PWD/.ruby-version
   chruby (cat $PWD/.ruby-version)
@@ -61,10 +66,3 @@ end
 
 bind \cr "rake"
 
-function ss -d "Run the script/server"
-	script/server
-end
-
-function sc -d "Run the Rails console"
-	script/console
-end
