4c13a3237e9a94f3cdf5d3264b6d1ef1d4a9efe9
Michaël van Diemen <van.diemen.michael@gmail.com>
2019-12-09 10:26:37 +0100

    Update screen script for docking station

--- .config/i3/i3_edp.sh
+++ .config/i3/i3_edp.sh
@@ -1,37 +1,22 @@
 #!/bin/bash
-if xrandr --query | grep "^HDMI1 connected"; then
-#    if xrandr --query | grep "3840x2160"; then
-#        xrandr --auto --output "HDMI1" --dpi 120 --mode "3840x2160" --output "eDP1" --off --auto
-#    else
-        xrandr --auto --output "HDMI1" --dpi 96 --mode "2560x1440" --output "eDP1" --off --auto
-#    fi
-    # /usr/local/libexec/hotplug_polybar.sh
-    /usr/bin/notify-send --urgency=low -t 500 "Switched to HDMI1"
-elif xrandr --query | grep "^DP1 connected"; then
-    xrandr --auto --output "DP1" --dpi 96 --mode "2560x1440" --output "eDP1" --off --auto
-    # /usr/local/libexec/hotplug_polybar.sh
-    /usr/bin/notify-send --urgency=low -t 500 "Switched to DP1"
-elif xrandr --query | grep "^DP2 connected"; then
-    xrandr --auto --output "DP2" --dpi 96 --mode "2560x1440" --output "eDP1" --off --auto
-    # /usr/local/libexec/hotplug_polybar.sh
-    /usr/bin/notify-send --urgency=low -t 500 "Switched to DP2"
+SIZE="2560x1440"
+if xrandr | grep "^HDMI1 connected"; then
+  DISPLAY="HDMI1"
+elif xrandr | grep "^DP1 connected"; then
+  DISPLAY="DP1"
+elif xrandr | grep "^DP2 connected"; then
+  DISPLAY="DP2"
+elif xrandr | grep "^DP2-3 connected"; then
+  DISPLAY="DP2-3"
+elif xrandr | grep "^DP1-3 connected"; then
+  DISPLAY="DP1-3"
 else
-  xrandr --auto --output "^eDP1" --dpi 120 --mode "2560x1440"
-  # /usr/local/libexec/hotplug_polybar.sh
-  /usr/bin/notify-send --urgency=low -t 500 "Switched to eDP1"
+  DISPLAY="eDP1"
 fi
 
-# if xrandr --query | grep "HDMI-1 connected"; then
-#     # xrandr --auto --output "HDMI-1" --dpi 120 --mode "3840x2160" --output "eDP-1" --off --auto
-#     xrandr --auto --output "HDMI-1" --dpi 96 --mode "2560x1440" --output "eDP-1" --off --auto
-#     # /usr/local/libexec/hotplug_polybar.sh
-#     /usr/bin/notify-send --urgency=low -t 500 "Switched to HDMI-1"
-# elif xrandr --query | grep "DP-2 connected"; then
-#     xrandr --auto --output "DP-2" --dpi 96 --mode "2560x1440" --output "eDP-1" --off --auto
-#     # /usr/local/libexec/hotplug_polybar.sh
-#     /usr/bin/notify-send --urgency=low -t 500 "Switched to DP-2"
-# else
-#   xrandr --auto --output "eDP-1" --dpi 120 --mode "2560x1440"
-#   # /usr/local/libexec/hotplug_polybar.sh
-#   /usr/bin/notify-send --urgency=low -t 500 "Switched to eDP-1"
-# fi
+if [ "$DISPLAY" == "eDP1" ]; then
+  xrandr --auto --output "^eDP1" --dpi 120 --mode "2560x1440"
+else
+  xrandr --auto --output $DISPLAY --dpi $DPI --mode $SIZE --output "eDP1" --off --auto
+    /usr/bin/notify-send --urgency=low -t 500 "Switched to $DISPLAY"
+fi
