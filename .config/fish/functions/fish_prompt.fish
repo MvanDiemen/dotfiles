7e9da34ebcc952316f00739cbebf34a455109c74
Michaël van Diemen <van.diemen.michael@gmail.com>
2019-12-24 19:20:55 +0100

    Update prompt to show timestamp

--- .config/fish/functions/fish_prompt.fish
+++ .config/fish/functions/fish_prompt.fish
@@ -1,6 +1,13 @@
 set fish_git_dirty_color red
 set fish_git_not_dirty_color green
 
+set -g c0 (set_color 005284)
+set -g c1 (set_color 0075cd)
+set -g c2 (set_color 009eff)
+set -g c3 (set_color 6dc7ff)
+set -g c4 (set_color ffffff)
+set -g ce (set_color $fish_color_error)
+
 function parse_git_branch
   set -l branch (git branch 2> /dev/null | grep -e '\* ' | sed 's/^..\(.*\)/\1/')
   set -l git_diff (git diff)
@@ -15,8 +22,10 @@ end
 function fish_prompt
   set -l git_dir (git rev-parse --git-dir 2> /dev/null)
   if test -n "$git_dir"
+    printf (date "+$c1%H$c1:$c1%M ")
     printf '%s%s%s:%s> ' (set_color $fish_color_cwd) (prompt_pwd) (set_color normal) (parse_git_branch)
   else
+    printf (date "+$c1%H$c1:$c1%M ")
     printf '%s%s%s> ' (set_color $fish_color_cwd) (prompt_pwd) (set_color normal)
   end
 end
